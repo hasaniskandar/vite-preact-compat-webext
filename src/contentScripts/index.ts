837cb88260f7b161b476d8678216161b6e4fbbb9
Hasan Iskandar <hasan.iskandar@gmail.com>
2022-02-28 13:04:26 +0700

    chore: log with dynamic package name

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -3,15 +3,16 @@ import { onMessage } from 'webext-bridge'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './views/App'
+import { name as packageName } from '../../package.json'
 import '../styles'
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 ;(() => {
-  console.info('[vitesse-webext] Hello world from content script')
+  console.info(`[${packageName}] Hello world from content script`)
 
   // communication example: send previous tab title from background page
   onMessage('tab-prev', ({ data }) => {
-    console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
+    console.log(`[${packageName}] Navigate from page "${data.title}"`)
   })
 
   // mount component to context window
